632277729421ecea1fec5fbc03ca0a96f55b2551
james-door <103546187+james-door@users.noreply.github.com>
2023-12-12 14:29:28 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Set the background color of the website to blue
+    document.body.style.backgroundColor = 'blue';
+
+    // The rest of your JavaScript code goes here
+
+    // Example: Select elements, add event listeners, manipulate the DOM, etc.
+});
