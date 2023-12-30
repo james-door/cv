2b4e2ad809ef41b8cae40f36d465e9333c1db788
James <james.wood0042@gmail.com>
2023-12-30 17:41:34 +1000

    gatsby

--- docs/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Set the background color of the website to blue
-    document.body.style.backgroundColor = 'red';
-
-    // The rest of your JavaScript code goes here
-
-    // Example: Select elements, add event listeners, manipulate the DOM, etc.
-});
