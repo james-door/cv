779e83c9ae8b216d7f1831d8e6f535f0a9ed0d2a
James <james.wood0042@gmail.com>
2024-01-11 12:44:22 +1000

    Attempt to fix mounting.

--- src/componets/Layout.js
+++ src/componets/Layout.js
@@ -8,8 +8,9 @@ import { Link } from 'gatsby';
 
 
 export default function Layout({children}) {
-const [darkModeState,setDarkMode] = useState(localStorage.getItem('darkModeState'));
+const inBrowser = typeof window !== 'undefined';
 
+const [darkModeState,setDarkMode] = useState(inBrowser ? localStorage.getItem('darkModeState') : null);
 useEffect(() => {
   if (darkModeState !== null) {
     document.documentElement.setAttribute('colour-theme', darkModeState);
@@ -17,16 +18,17 @@ useEffect(() => {
 }, [darkModeState]);
 
 
-if(darkModeState == null){
-  localStorage.setItem('darkModeState','light');
-  setDarkMode('light');
-}
 
 const ChangeDarkMode = ()=>{  
   const newState = darkModeState === 'light' ? 'dark' : 'light';
   setDarkMode(newState);
   localStorage.setItem('darkModeState',newState);
   console.log(newState);
+}
+
+if(inBrowser && darkModeState == null){
+  localStorage.setItem('darkModeState','light');
+  setDarkMode('light');
 }
 
 
